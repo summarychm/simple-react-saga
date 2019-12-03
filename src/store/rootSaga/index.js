b22fa874c6b2e4dcd4b1798fb28228e20ba27984
max.liu <summarychm@hotmail.com>
2019-12-03 13:06:48 +0800

    📦 添加saga功能示例

--- src/store/rootSaga/index.js
+++ src/store/rootSaga/index.js
@@ -1,3 +1,11 @@
+import { ASYNC_INCREMENT } from "../action-types";
+import { increment } from "../actions";
+import { take, put, delay } from "redux-saga/effects";
 export default function* rootSaga() {
 	console.log("rootSaga run!");
+	for (let i = 0; i < 3; i++) {
+		yield take(ASYNC_INCREMENT);
+		const value = yield delay(500, 10);
+		yield put(increment(value));
+	}
 }
